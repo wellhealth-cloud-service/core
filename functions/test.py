18cf64f8462f29972e0ff4c68bfca2811c35a3b5
Saeed Rahmani <saeedrahmo@gmail.com>
2021-12-01 17:51:33 -0500

    performance test done

--- functions/test.py
+++ functions/test.py
@@ -1,10 +1,9 @@
-
 from telethon import TelegramClient
 
 # Remember to use your own values from my.telegram.org!
-api_id = 14367775
-api_hash = '86c2b7287a41f08329a15f7b15188df6'
-phone = '+14384934321'
+api_id = *
+api_hash = '*'
+phone = '*'
 client = TelegramClient('anon', api_id, api_hash)
 
 async def main():
@@ -12,8 +11,14 @@ async def main():
     if not await client.is_user_authorized():
         await client.send_code_request(phone)
         me =await client.sign_in(phone, input('Enter code: '))
-        print(me.stringify())
+    
+    # Getting information about yourself
+    me = await client.get_me()
+    #print(me.stringify())
         #await client.sign_in(phone, cod)
+    # You can send messages to yourself...
+    for x in range(1000):
+        await client.send_message('wellhealthbot', '/analysis')
 
 with client:
     client.loop.run_until_complete(main())
